5d189c60c89190cb5ae8445d6ebc7c4b3d9c028a
Louis Coleman <coleloui18@gmail.com>
2021-03-08 14:44:49 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
+
+  let sum = (a:number, b:number):number => {
+    return a + b
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {sum(1, 15)}
         </p>
         <a
           className="App-link"
